630f9cc6426fb5bcec4975f2ddc1518e5c3ada6c
Ynot-Velney <59128608+Ynot-Velney@users.noreply.github.com>
2019-12-22 11:56:07 +0800

    Update case.js

--- miniprogram-5/pages/case/case.js
+++ miniprogram-5/pages/case/case.js
@@ -3,9 +3,9 @@ Page({
     device_name: 32,
 
   },
-
+//调用onenet api获取从arduino获取的数据值
   OnLoad: function (options) {
-    var _this = this;
+    var _this = this;//用onLoad函数直接刷新当前垃圾桶的垃圾深度
     //console.log(options.device_id);// 打印设备ID
     //console.log(app.data.api_key);// 打印API_KEY
     wx.request({
@@ -14,10 +14,10 @@ Page({
         'api-key': 'Rh3VomUuIy1yXEpJURmUlx7npRE='
       },
       success: function (res) {
-        console.log(res.data);
+        console.log(res.data);//在控制台上打印
         _this.setData(
           {
-            device_name: res.data.data.datastreams[0].datapoints[0].value
+            device_name: res.data.data.datastreams[0].datapoints[0].value//获取数据值后将其传送到data
 
           });
       },
@@ -26,7 +26,7 @@ Page({
           title: '与服务器通信失败',
           icon: 'fail',
           duration: 2000
-        })
+        })//与服务器无法连接时提醒
       }
     })
   },
